15fa2e835a6251a7a4bba246b7c23b7cee4a58b9
Razarik <48281364+Razarik@users.noreply.github.com>
2020-10-01 15:09:52 +0200

    startup project

---
+++ src/test/java/dist/ai/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dist.ai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
